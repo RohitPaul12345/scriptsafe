b33f90632865741e6aa22d727dac311f996032fb
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-30 11:49:45 +0530

    Update webrtctest.js

--- js/webrtctest.js
+++ js/webrtctest.js
@@ -2,8 +2,8 @@
 // Distributed under the terms of the GNU General Public License
 // The GNU General Public License can be found in the gpl.txt file. Alternatively, see <http://www.gnu.org/licenses/>.
 'use strict';
-var rtcstatus = null;
-var rtctest = self.RTCPeerConnection || self.webkitRTCPeerConnection;
+let rtcstatus = null;
+let rtctest = self.RTCPeerConnection || self.webkitRTCPeerConnection;
 try {
 	if (rtctest) rtcstatus = new rtctest(null);
 } catch (exception) {
